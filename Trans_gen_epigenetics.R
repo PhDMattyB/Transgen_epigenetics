d09d9630c0432f6e0d026063550ca664757259bf
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-10-07 17:23:05 +0100

    Update Trans_gen_epigenetics.R

--- Trans_gen_epigenetics.R
+++ Trans_gen_epigenetics.R
@@ -14,4 +14,16 @@ library(tidyverse)
 
 methy = read_rds(file = "Methylated.cov.noZero.rds")
 
+methy %>% 
+  as.data.frame() %>% 
+  write_csv('Methylated_cov_nozero.csv')
+
 unmethy = read_rds(file = 'UnMethylated.cov.noZero.rds')
+
+unmethy %>% 
+  as.data.frame() %>% 
+  write_csv('Unmethylated_cov_nozero.csv')
+
+## read new data format to make sure this actually worked
+
+methy = read_csv('Methylated_cov_nozero.csv')
