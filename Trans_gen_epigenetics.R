db28e09cd98d5a434e5c6ede6968203d1fa70e24
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-10-08 11:45:18 +0100

    Update Trans_gen_epigenetics.R

--- Trans_gen_epigenetics.R
+++ Trans_gen_epigenetics.R
@@ -12,50 +12,48 @@
 setwd('~/Methylation_data/')
 
 library(tidyverse)
-library(devtools)
-install.packages('freeR')
-install_github('cfree14/freeR')
 
-library(freeR)
-
-methy_rds = read_rds(file = "Methylated.cov.noZero.rds")
-head(methy_rds)
-
-methy_rds %>% 
-  # as.matrix() %>% 
-  # rownames_to_column(var = 'Location') %>% 
-  as.data.frame() %>%
-  as_tibble() %>% 
-  # rowid_to_column(var = 'rowid')
-  write_csv('Methylated_cov_nozero.csv')
-
-
-row.names(methy_rds) %>% 
-  as.data.frame() %>% 
-  as.tibble() %>% 
-  rename(Location_data = 1) %>% 
-  write_csv('Methylation_location_data.csv')
-
-  
-unmethy_rds = read_rds(file = 'UnMethylated.cov.noZero.rds')
-
-unmethy %>% 
-  as.data.frame() %>% 
-  write_csv('Unmethylated_cov_nozero.csv')
-
-row.names(unmethy_rds) %>% 
-  as.data.frame() %>% 
-  as.tibble() %>% 
-  rename(Location_data = 1) %>% 
-  write_csv('UnMethylation_location_data.csv')
+# methy_rds = read_rds(file = "Methylated.cov.noZero.rds")
+# head(methy_rds)
+# 
+# methy_rds %>% 
+#   # as.matrix() %>% 
+#   # rownames_to_column(var = 'Location') %>% 
+#   as.data.frame() %>%
+#   as_tibble() %>% 
+#   # rowid_to_column(var = 'rowid')
+#   write_csv('Methylated_cov_nozero.csv')
+# 
+# 
+# row.names(methy_rds) %>% 
+#   as.data.frame() %>% 
+#   as.tibble() %>% 
+#   rename(Location_data = 1) %>% 
+#   write_csv('Methylation_location_data.csv')
+# 
+#   
+# unmethy_rds = read_rds(file = 'UnMethylated.cov.noZero.rds')
+# 
+# unmethy %>% 
+#   as.data.frame() %>% 
+#   write_csv('Unmethylated_cov_nozero.csv')
+# 
+# row.names(unmethy_rds) %>% 
+#   as.data.frame() %>% 
+#   as.tibble() %>% 
+#   rename(Location_data = 1) %>% 
+#   write_csv('UnMethylation_location_data.csv')
 
 
 ## read new data format to make sure this actually worked
 
 methy = read_csv('Methylated_cov_nozero.csv')
-
 Loc_data = read_csv('Methylation_location_data.csv')
-
 methy_data = bind_cols(Loc_data, 
                        methy)
 
+unmethy = read_csv('Unmethylated_cov_nozero.csv')
+unmethy_loc = read_csv('UnMethylation_location_data.csv')
+
+unmenthy_data = bind_cols(unmethy_loc, 
+                          unmethy)
