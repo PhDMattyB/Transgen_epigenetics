8519749cf7abf5fa2d3a17ba79d91d987879f61a
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-11-12 13:46:39 +0000

    Update Isolating_phenotypes.R

--- Isolating_phenotypes.R
+++ Isolating_phenotypes.R
@@ -9,9 +9,23 @@
 
 
 setwd('~/Parsons_Postdoc/Methylation_data/')
+setwd('~/Methylation_data/')
 
 library(tidyverse)
 library(geomorph)
+
+
+# metadata ----------------------------------------------------------------
+
+meta_data = read_csv('formattedDataEU.csv') %>% 
+  select(fish, 
+             ID, 
+             Full_ID, 
+             F1, 
+             F2, 
+             poppair, 
+             ecotype...12, 
+         csize_real)
 
 
 # Body shape data ---------------------------------------------------------
@@ -20,9 +34,30 @@ bs_data = readland.tps('F2_All_aligned_withsliders.tps',
                        specID = 'imageID', 
                        readcurves = T)
 
-sliders = define.sliders(bs_data, 
-               10)
+sliders = define.sliders(c(28:37,1))
 
 gpa = gpagen(bs_data, 
              print.progress = T, 
              curves = sliders)
+
+
+
+# test for allometry ------------------------------------------------------
+
+allo_mod = procD.lm(gpa$coords ~ log(meta_data$csize_real), 
+                    iter = 999, 
+                    RRPP = T)
+
+summary(allo_mod)
+
+## Allometry is not significant. 
+
+
+# mean shape data ---------------------------------------------------------
+
+mean_shape = mshape(gpa$coords)
+matrix_mean_shape = as.matrix(mean_shape)
+mean_shape_array = array(matrix_mean_shape, 
+                         dim = c(37, 2, 1))
+
+
