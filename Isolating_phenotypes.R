a3cb8d751b082fce9f9d7041bb18a3834e97bde2
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-11-12 11:39:52 +0000

    phenotype wrangling

---
+++ Isolating_phenotypes.R
@@ -0,0 +1,15 @@
+##############################
+## Meth paper - transgenerational phenotypes
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 12.11.2024
+##
+##############################
+
+
+setwd('~/Parsons_Postdoc/Methylation_data/')
+
+library(tidyverse)
+library(geomorph)
+
