d3f0a588c93adb369263d445cf020c855461c8d1
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-11-12 18:50:46 +0000

    Update Isolating_phenotypes.R

--- Isolating_phenotypes.R
+++ Isolating_phenotypes.R
@@ -113,3 +113,52 @@ for(i in 1:1575){
 #               file = 'F1_effect_landmarks_all_individuals.tps',
 #               scale = NULL, 
 #               specID = T)
+
+
+
+# Isolate F2 effects ------------------------------------------------------
+
+
+F2_temp_mod = procD.lm(gpa$coords ~ meta_data$F2, 
+                       iter = 999, 
+                       RRPP = T)
+
+
+F2_fitted_12deg = F2_temp_mod$GM$fitted[,,1]
+F2_fitted_mat_12deg = as.matrix(F2_fitted_12deg)
+F2_12deg_array = array(F2_fitted_mat_12deg, 
+                       dim = c(37, 2, 1))
+
+F2_fitted_18deg = F2_temp_mod$GM$fitted[,,35]
+F2_fitteed_18deg_mat = as.matrix(F2_fitted_18deg)
+F2_18deg_array = array(F2_fitteed_18deg_mat, 
+                       dim = c(37, 2, 1))
+
+F2_12deg_range = c(1:50, 105:155, 206:259, 310:359, 412:461, 512:563, 612:650, 701:746, 798:847, 898:947, 998:1047,
+                   1098:1147, 1198:1249, 1298:1347, 1398:1447, 1475:1525)
+
+F2_18deg_range = c(51:104, 156:205, 260:309, 360:411, 462:511, 564:611, 651:700, 747:797, 848:897, 948:997,
+                   1048:1097, 1148:1197, 1250:1297, 1348:1397, 1448:1474, 1525:1575)
+
+F2_array = array(0, dim = c(37, 2, 1575))
+
+for(i in F2_12deg_range){
+  F2_array[,,i] = gpa$coords[,,i] - F2_12deg_array[,,1]
+}
+
+for(i in F2_18deg_range){
+  F2_array[,,i] = gpa$coords[,,i] - F2_18deg_array[,,1]
+}
+
+
+F2_array_consensus = array(0, dim = c(37, 2, 1575))
+
+for(i in 1:1575){
+  F2_array_consensus[,,i] = F2_array[,,i] + mean_shape_array[,,1]
+}
+
+writeland.tps(F2_array_consensus,
+              file = 'F2_effect_landmarks_all_individuals.tps',
+              scale = NULL,
+              specID = T)
+
