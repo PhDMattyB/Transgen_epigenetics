7e5d11bfddade792b1a8fdc027c0c88918aacf81
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-11-12 11:40:29 +0000

    Update Isolating_phenotypes.R

--- Isolating_phenotypes.R
+++ Isolating_phenotypes.R
@@ -13,3 +13,16 @@ setwd('~/Parsons_Postdoc/Methylation_data/')
 library(tidyverse)
 library(geomorph)
 
+
+# Body shape data ---------------------------------------------------------
+
+bs_data = readland.tps('F2_All_aligned_withsliders.tps', 
+                       specID = 'imageID', 
+                       readcurves = T)
+
+sliders = define.sliders(bs_data, 
+               10)
+
+gpa = gpagen(bs_data, 
+             print.progress = T, 
+             curves = sliders)
