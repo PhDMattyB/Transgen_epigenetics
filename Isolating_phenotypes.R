216440d81d2d530d9d1ccd3dbf1a6e099f203059
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-11-12 18:11:12 +0000

    array ranges

--- Isolating_phenotypes.R
+++ Isolating_phenotypes.R
@@ -34,14 +34,14 @@ bs_data = readland.tps('F2_All_aligned_withsliders.tps',
                        specID = 'imageID', 
                        readcurves = T)
 
-bs_data = read_csv("epigenetic_landmark_data.csv") %>% 
-  select(-na)
-
-bs_data = as.matrix(bs_data)
-
-arrayspecs(A = bs_data, 
-           p = 37, 
-           k = 2)
+# bs_data = read_csv("epigenetic_landmark_data.csv") %>% 
+#   select(-na)
+# 
+# bs_data = as.matrix(bs_data)
+# 
+# arrayspecs(A = bs_data, 
+#            p = 37, 
+#            k = 2)
 
 
 sliders = define.sliders(c(28:37,1))
@@ -87,6 +87,11 @@ F1_fitted_18deg = F1_temp_mod$GM$fitted[,,64]
 F1_fitteed_18deg_mat = as.matrix(F1_fitted_18deg)
 F1_18deg_array = array(F1_fitteed_18deg_mat, 
                        dim = c(37, 2, 1))
+
+F1_12deg_range = c(1:104, 206:370, 411:511, 612:700, 789:897, 999:1097, 1198:1297, 1398:1474)
+
+F1_18deg_range = c(105:205, 371:410, 512:611, 701:788, 898:998, 1098:1197, 1298:1297, 1475:1575)
+
 
 
 
