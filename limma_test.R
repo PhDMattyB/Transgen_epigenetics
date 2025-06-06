ec4ce64765d118ea64c1cbd623c942a6ca12be81
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-06-06 15:50:32 +0100

    spring code cleaning

--- limma_test.R
+++ limma_test.R
@@ -9,27 +9,54 @@ library(qvalue)
 
 setwd('~/Methylation_data/')
 
-# setwd('C:/Users/mkb6d/Documents/')
 
-mvalues = read_csv('MVALUES_methylation_cleaned_data.csv')
-
-
-
+mvalues = read_csv('MVALUES_methylation_cleaned_data.csv') %>% 
+  select(-Location_data)
+meta_data = read_csv('Methylation_meta_data.csv')
 
 
+
+## making the metadata
 # 
-# meta_data = read_csv('formattedDataEU.csv') %>%
-#   select(fish,
-#          ID,
-#          Full_ID,
-#          F1,
-#          F2,
-#          poppair,
-#          ecotype...13,
-#          csize_real) %>%
-#   rename(ecotype = ecotype...13)
-# 
-# mval_small = read_csv('mvalues_chrI.csv')
+# mval_small_meta %>%
+#   separate(col = Location_data, 
+#            into = c('group1', 
+#                     'group2', 
+#                     'group3'), 
+#            sep = '_', 
+#            remove = F) %>% 
+#   dplyr::select(-group1) %>% 
+#   separate(col = group2, 
+#            into = c('individualID', 
+#                     'pop_temp'), 
+#            sep = '-', 
+#            remove = F) %>% 
+#   separate(col = group3, 
+#            into = c('EU_ID', 
+#                     'EU_individualID', 
+#                     'garbarge'), 
+#            sep = '-') %>% 
+#   select(-garbarge, 
+#          -group2) %>%
+#   extract(pop_temp, 
+#           into = c('poppair', 
+#                    'temps'), 
+#           '^([A-Z]+)(\\d+)$') %>% 
+#   mutate(len = str_length(temps), 
+#          half = len %/% 2, 
+#          F1_temp = str_sub(temps, 1, half), 
+#          F2_temp = str_sub(temps, 
+#                            half + 1, 
+#                            len)) %>% 
+#   dplyr::select(Location_data, 
+#                 individualID, 
+#                 poppair, 
+#                 temps, 
+#                 EU_ID, 
+#                 EU_individualID, 
+#                 F1_temp, 
+#                 F2_temp) %>% 
+#   write_csv('Methylation_meta_data.csv')
 # 
 # 
 # meta_fish_ID = meta_data %>%
@@ -59,272 +86,41 @@ mvalues = read_csv('MVALUES_methylation_cleaned_data.csv')
 #                         Fish_ID)) 
 # 
 
-  
-  # separate(Fish_ID,
-  #          into = c('ID',
-  #                   'ID2',
-  #                   'ID3'),
-  #          sep = '_',
-  #          remove = F) %>%
-  # separate(ID3,
-  #          into = c('symbol',
-  #                   'ID4',
-  #                   'num'),
-  #          sep = '',
-  #          remove = F) %>%
-  # select(-symbol) %>%
-  # mutate(new_vals = paste0('#G',
-  #                          str_pad(num,
-  #                                  0,
-  #                                  side = 'left'))) %>%
-  # select(-ID3,
-  #        -ID4,
-  #        -num) %>%
-  # unite(Fish_ID2,
-  #       c('ID',
-  #         'ID2',
-  #         'new_vals'),
-  #       sep = '_') %>%
-  # select(Fish_ID2) %>%
-  # rename(Fish_ID = Fish_ID2)
-
-# meta_data_cleaned = bind_cols(meta_fish_ID,
-#                               meta_data) %>%
-#   arrange(Fish_ID)%>%
-#   filter(grepl('#G', Fish_ID))
-
-
-# 
-meta_data_cleaned = read_csv('Cleaned_meta_data.csv') %>%
-  arrange(Fish_ID)
-
-
-### small data
-
-# mval_small_ID = mval_small %>% 
-#   select(Location_data)
-# 
-# methy_fish_ID = mval_small_ID %>% 
-#   # separate(Location_data, 
-#   #         into = c('garbage', 
-#   #            'ID'), 
-#   #          sep = '-') %>% 
-#   separate_wider_regex(Location_data, 
-#                        c(var1 = ".*?", 
-#                          "-", 
-#                          var2 = ".*")) %>% 
-#   select(var2) %>% 
-#   separate(var2, 
-#            into = c('ID', 
-#                     'ID2'), 
-#            sep = '-') %>% 
-#   mutate(new_vals = paste0('#G', 
-#                            str_pad(ID2, 
-#                                    0, 
-#                                    side = 'left'))) %>% 
-#   select(ID, 
-#          new_vals) %>% 
-#   unite(col = Fish_ID, 
-#         sep = '_') %>% 
-#   arrange(Fish_ID) 
-# 
-# mvals_cleaned = bind_cols(methy_fish_ID, 
-#                           mval_small) %>% 
-#   select(-Location_data) %>% 
-#   arrange(Fish_ID)
-# 
-# 
-# meta_data_cleaned = meta_data_cleaned %>% 
-#   separate(Fish_ID, 
-#            into = c('G', 
-#                     'num'), 
-#            sep = '#') %>% 
-#   filter(num %in% c('G1', 
-#                     'G2', 
-#                     'G3', 
-#                     'G4', 
-#                     'G5')) %>% 
-#   unite(Fish_ID, 
-#         c('G', 
-#           'num'), 
-#         sep = '#') %>% 
-#   arrange(Fish_ID)
-# 
-# methy_fish_ID = inner_join(meta_data_cleaned, 
-#            methy_fish_ID, 
-#            by = 'Fish_ID')
-
-
-## big data
-mval_ID = mvalues %>% 
-  dplyr::select(Location_data)
-
-methy_fish_ID = mval_ID %>% 
-  # separate(Location_data, 
-  #         into = c('garbage', 
-  #            'ID'), 
-  #          sep = '-') %>% 
-  separate_wider_regex(Location_data, 
-                       c(var1 = ".*?", 
-                         "-", 
-                         var2 = ".*")) %>% 
-  dplyr::select(var2) %>% 
-  separate(var2, 
-           into = c('ID', 
-                    'ID2'), 
-           sep = '-') %>% 
-  mutate(new_vals = paste0('#G', 
-                           str_pad(ID2, 
-                                   0, 
-                                   side = 'left'))) %>% 
-  dplyr::select(ID, 
-         new_vals) %>% 
-  unite(col = Fish_ID, 
-        sep = '_') %>% 
-  arrange(Fish_ID) 
-
-mvalues = bind_cols(methy_fish_ID, 
-                          mvalues) %>% 
-  dplyr::select(-Location_data) %>% 
-  arrange(Fish_ID)
-
-
-meta_data_cleaned = meta_data_cleaned %>% 
-  separate(Fish_ID, 
-           into = c('G', 
-                    'num'), 
-           sep = '#') %>% 
-  filter(num %in% c('G1', 
-                    'G2', 
-                    'G3', 
-                    'G4', 
-                    'G5')) %>% 
-  unite(Fish_ID, 
-        c('G', 
-          'num'), 
-        sep = '#') %>% 
-  arrange(Fish_ID)
-
-methy_fish_ID = inner_join(meta_data_cleaned, 
-                           methy_fish_ID, 
-                           by = 'Fish_ID')
-meta_data_cleaned$Fish_ID == methy_fish_ID$Fish_ID
-
-## organized data. 
-
-## Finally cleaned
-methy_fish_ID = read_csv("Methylation_meta_data_issue.csv") %>% 
-  arrange(Fish_ID)
-
-
-methy_fish_ID$F1 = factor(methy_fish_ID$F1)
-methy_fish_ID$F2 = factor(methy_fish_ID$F2)
-methy_fish_ID$ecotype = factor(methy_fish_ID$ecotype)
-methy_fish_ID$poppair = factor(methy_fish_ID$poppair)
-
-design_formula = ~0+F1*F2*ecotype*poppair
-
-design = model.matrix(design_formula, data = methy_fish_ID)
-
-
-design_formula2 = ~0+F1*F2*ecotype
-
-design2 = model.matrix(design_formula2, data = methy_fish_ID)
-
-# print(design)
 
 set.seed(1738)
 
-trans_methy = mvals_cleaned %>%
-  select(-Fish_ID) %>% 
-  # remove_rownames() %>% 
-  # select(-fish, 
-  #        -ID, 
-  #        -Full_ID, 
-  #        -F1, 
-  #        -F2, 
-  #        -poppair, 
-  #        -ecotype, 
-  #        -csize_real) %>% 
-  # row.names(.)
-  # rownames_to_column(var = 'Fish_ID') %>% 
-  t() %>% 
-  as_tibble()
+## 
+# trans_methy = mvals_cleaned %>%
+#   select(-Fish_ID) %>% 
+#   # remove_rownames() %>% 
+#   # select(-fish, 
+#   #        -ID, 
+#   #        -Full_ID, 
+#   #        -F1, 
+#   #        -F2, 
+#   #        -poppair, 
+#   #        -ecotype, 
+#   #        -csize_real) %>% 
+#   # row.names(.)
+#   # rownames_to_column(var = 'Fish_ID') %>% 
+#   t() %>% 
+#   as_tibble()
   
-mvals_cleaned %>% 
-  select(1) %>% 
-  write_tsv('mvals_small_colnames.txt')
-
-names(mval_small) %>% 
-  as_tibble() %>% 
-  slice(-1) %>% 
-  write_tsv('mvals_small_chr_rownames.txt')
-
-fit = lmFit(trans_methy, design2)
-
-ebayes_fit = eBayes(fit)
-
-topTable(ebayes_fit) %>%
-  rownames_to_column() %>% 
-  as_tibble() %>% 
-  write_csv('Refined_model_Top_table_example_df_CHRI_methylation.csv')
-
-  
-ordinary.t = fit$coefficients/fit$stdev.unscaled/fit$sigma 
-
-
-head(ordinary.t)
-
-# ebayes_fit$p.value %>% 
-#   as.data.frame() %>% 
+# mvals_cleaned %>% 
+#   select(1) %>% 
+#   write_tsv('mvals_small_colnames.txt')
+# 
+# names(mval_small) %>% 
 #   as_tibble() %>% 
-#   arrange(`F118:F218:ecotypew`)
-
+#   slice(-1) %>% 
+#   write_tsv('mvals_small_chr_rownames.txt')
+# 
 
 
 
 # glmmseq -----------------------------------------------------------------
 
-library(glmmSeq)
-
-
-disp = apply(trans_methy, 1, function(x){
-  (var(x, na.rm = T)-mean(x, na.rm = T))/(mean(x, na.rm = T)**2)
-})
-
-meta_test = methy_fish_ID %>%
-  as.data.frame() %>%
-  select(Fish_ID,
-         F1,
-         F2,
-         ecotype,
-         poppair)
 # 
-# test_mvals = trans_methy %>% 
-#   as.data.frame() %>% 
-#   mutate(across(.cols = c(V1:V595), 
-#                 .fns = ~if_else(. == ., 1, 0)))
-# 
-# 
-# 
-# 
-# glmm = glmmSeq(~ F1 * F2 * ecotype + (1|poppair), 
-#         countdata = trans_methy, 
-#         metadata = meta_test,
-#         method = 'glmmTMB', 
-#         returnList = T)
-# 
-# summary(glmm)
-# 
-# glmm = lmmSeq(~ F1 * F2 * ecotype + (1|poppair), 
-#                maindata = test_mvals, 
-#                metadata = meta_test)
-# 
-# 
-# 
-# DGEList(trans_methy)
-# calcNormFactors(trans_methy)
-
 test_mvals = mvals_cleaned %>% 
   dplyr::select(-Fish_ID) %>% 
   as.data.frame()
@@ -336,30 +132,18 @@ library(glmmTMB)
 model_results_table = as.data.frame(matrix(nrow = 191662, 
                                           ncol = 34))
 
-# gene = test_mvals[,1]
-# # all_others = rowSums(test_mvals[,-i])
-# # Y = cbind(gene, 
-# # all_others)
-# 
-# Model = glmmTMB(gene ~ F1 * F2 * ecotype + (1|poppair), 
-#                 # family = gaussian(), 
-#                 # control = glmerControl(
-#                 # optimizer = 'optimx', optCtrl = list(method = 'nlminb')), 
-#                 data = meta_test)
-# 
-# model_results = summary(Model)
 
-for(i in 1:ncol(test_mvals)){
-  gene = test_mvals[,i]
+for(i in 1:ncol(mvalues)){
+  gene = mvalues[,i]
   # all_others = rowSums(test_mvals[,-i])
   # Y = cbind(gene, 
             # all_others)
   
-  Model = glmmTMB(gene ~ F1 * F2 * ecotype + (1|poppair), 
+  Model = glmmTMB(gene ~ F1_temp * F2_temp * ecotype + (1|poppair), 
                 # family = gaussian(), 
                 # control = glmerControl(
                   # optimizer = 'optimx', optCtrl = list(method = 'nlminb')), 
-                data = meta_test)
+                data = meta_data)
   
   model_results = summary(Model)
   
@@ -400,19 +184,6 @@ for(i in 1:ncol(test_mvals)){
   model_results_table[i, 34] = model_results$coefficients$cond[8,4]
   
   
-}
-
-for(i in 1:ncol(test_mvals)){
-  gene = test_mvals[,i]
-  all_others = rowSums(test_mvals[,-i])
-  Y = cbind(gene,
-            all_others)
-  
-  Model2 = glmer(Y ~ F1 * F2 * ecotype + (1|poppair),
-                 family = binomial(),
-                 control = glmerControl(
-                   optimizer = 'optimx', optCtrl = list(method = 'nlminb')),
-                 data = meta_test)
 }
 
 
